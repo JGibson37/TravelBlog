0556d66280a2d5823223a3b7b852248422daafc7
Gale King <grexkingiv@gmail.com>
2020-06-12 14:58:54 -0400

    Solved Template Problem

--- src/main/java/org/wecancodeit/reviews/CityController.java
+++ src/main/java/org/wecancodeit/reviews/CityController.java
@@ -13,6 +13,8 @@ public class CityController {
 
     Map<String,City> cities = new HashMap<>();
 
+
+
     public CityController(){
         cities.put("Bruce Peninsula",new City("Bruce Peninsula","5 / 5","Bruce Peninsula National Park","Fish & Chip Place","The Bruce Peninsula is an astounding area to visit if you enjoy hiking and\n" +
                 "      nature. The winding pine forest trails and towering cliffs into crystal\n" +
@@ -24,7 +26,7 @@ public class CityController {
     @RequestMapping("cities/{cityName}")
     public String showSingleCity(@PathVariable String cityName, Model model){
       model.addAttribute("city",cities.get(cityName));
-      return "city-template";
+      return "cities-template";
     }
 
 }
---
+++ src/main/java/org/wecancodeit/reviews/CityStorage.java
@@ -0,0 +1,14 @@
+package org.wecancodeit.reviews;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+
+public class CityStorage {
+    Map<String, City> cities = new HashMap<>();
+    
+}
