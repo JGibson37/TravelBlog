e7a83f78e90b2594e1c1f7870a220da20b09fc04
Gale King <grexkingiv@gmail.com>
2020-06-12 10:03:52 -0400

    Added beginnings of book template

---
+++ src/main/java/org/wecancodeit/reviews/City.java
@@ -0,0 +1,35 @@
+package org.wecancodeit.reviews;
+
+public class City {
+
+    String cityName;
+    String starRating;
+    String pointOfInterest1;
+    String pointOfInterest2;
+    String review;
+
+    public City(String cityName, String starRating, String pointOfInterest1, String pointOfInterest2, String review) {
+        this.cityName = cityName;
+        this.starRating = starRating;
+        this.pointOfInterest1 = pointOfInterest1;
+        this.pointOfInterest2 = pointOfInterest2;
+        this.review = review;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+    public String getStarRating() {
+        return starRating;
+    }
+    public String getPointOfInterest1() {
+        return pointOfInterest1;
+    }
+    public String getPointOfInterest2() {
+        return pointOfInterest2;
+    }
+    public String getReview() {
+        return review;
+    }
+
+}
---
+++ src/main/java/org/wecancodeit/reviews/Country.java
@@ -0,0 +1,8 @@
+package org.wecancodeit.reviews;
+
+public class Country {
+
+    private String countryName;
+
+
+}
