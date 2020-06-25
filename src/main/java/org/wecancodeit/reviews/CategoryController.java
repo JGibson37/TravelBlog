fce6de34c63de908b21fbf10ea24482822c2bd73
Christin Banerjee <Christinprice5@gmail.com>
2020-06-25 15:38:01 -0400

    final- presentation ready! Great job teamgit add .

--- src/main/java/org/wecancodeit/reviews/CategoryController.java
+++ src/main/java/org/wecancodeit/reviews/CategoryController.java
@@ -2,8 +2,6 @@ package org.wecancodeit.reviews;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,9 +16,9 @@ public class CategoryController {
     }
 
     @RequestMapping({"/category"})
-    public String routeToCountries(Model model){
-        model.addAttribute("countries",countryRepo.findAll());
-        model.addAttribute("hashtags",hashtagRepo.findAll());
+    public String routeToCountries(Model model) {
+        model.addAttribute("countries", countryRepo.findAll());
+        model.addAttribute("hashtags", hashtagRepo.findAll());
         return "category-template";
     }
 
